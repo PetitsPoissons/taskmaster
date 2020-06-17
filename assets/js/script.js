1c0497758478a49c6776edffb7ca2665e0d83d82
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-06-16 18:23:14 -0700

    added drag-drop using jquery UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,7 +137,6 @@ $(".list-group").on("blur", "input[type='text']", function () {
   $(this).replaceWith(taskSpan);
 });
 
-
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -172,6 +171,56 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// convert lists into sortables
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    //console.log("activate", this);
+  },
+  deactivate: function(event) {
+    //console.log("deactivate", this);
+  },
+  over: function(event) {
+    //console.log("over", event.target);
+  },
+  out: function(event) {
+    //console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the taskdata in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -179,6 +228,21 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+// drop a task to trash zone
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    //console.log("over");
+  },
+  out: function(event, ui) {
+    //console.log("out");
+  }
 });
 
 // load tasks for the first time
